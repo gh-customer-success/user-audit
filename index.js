122f950948accbb1b2fddff900014efa1a911897
Rudy Sarmiento <rudy750@github.com>
2023-03-24 21:24:42 -0400

    fixed path

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const { graphql } = require('@octokit/graphql');
 const { gql } = require('graphql-tag');
 const fs = require('fs');
 
-const query = gql(fs.readFileSync('./audit.gql', 'utf8'));
-
+const query = gql(fs.readFileSync('audit.gql', 'utf8'));
 
 // most @actions toolkit packages have async methods
 async function run() {

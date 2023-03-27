f2d3a92f30123085093c94536f27aaf7b001e8aa
Rudy Sarmiento <rudy750@github.com>
2023-03-26 22:39:33 -0400

    fixed export of functions

--- dist/index.js
+++ dist/index.js
@@ -7728,61 +7728,66 @@ var external_fs_ = __nccwpck_require__(5747);
 // EXTERNAL MODULE: external "path"
 var external_path_ = __nccwpck_require__(5622);
 ;// CONCATENATED MODULE: ./sort-audit.js
-function getTeams(response) {
-    //get the data from the response
-    const data = response;
-    //create an object with the teams  their members and repositories
-    const teams = {};
-    const teamPermissions={};
-    //iterate through the teams
-    data.organization.teams.edges.forEach((team) => {
-        //get the team name
-        const teamName = team.node.name;
-      
-        //get the team members
-        teamPermissions.members = team.node.members.edges.map((member) => member.node.login);
-        //get the team repositories
-        teamPermissions.repositories = team.node.repositories.edges.map((repository) => repository.node.name);
-        
-        //add the team name and members to the teams object
-        teams[teamName] = teamPermissions;
-    }); 
-    return teams;
-  }
-//   import { createRequire } from 'node:module';
-//   const require = createRequire(import.meta.url);
-  
-//   const response = require('./response.json');
 
-  //a function that gets all the repositores and returns their collaborators and their permissions
-    function getRepos(response) {
-    //get the data from the response
-    const data = response;
-        
-    //create an object with the repositories and their collaborators
-    const repos = {};
-    //iterate through the repositories
-    data.organization.repositories.edges.forEach((repo) => {
-      //get the repository name
-      const repoName = repo.node.name;
-      //create an object with the collaborators and their permissions
-        const repoCollaborators = {};
-        //iterate through the collaborators
-        repo.node.collaborators.edges.forEach((collaborator) => {
-        //get the collaborator name
-        const collaboratorName = collaborator.node.login;
-        //get the collaborator permissions
-        const collaboratorPermissions = collaborator.permission;
-        //add the collaborator name and permissions to the collaborators object
-        repoCollaborators[collaboratorName] = collaboratorPermissions;
+   const getTeams = (response)=>{
+        //get the data from the response
+        const data = response;
+        //create an object with the teams  their members and repositories
+        const teams = {};
+        const teamPermissions = {};
+        //iterate through the teams
+        data.organization.teams.edges.forEach((team) => {
+            //get the team name
+            const teamName = team.node.name;
+
+            //get the team members
+            teamPermissions.members = team.node.members.edges.map((member) => member.node.login);
+            //get the team repositories
+            teamPermissions.repositories = team.node.repositories.edges.map((repository) => repository.node.name);
+
+            //add the team name and members to the teams object
+            teams[teamName] = teamPermissions;
         });
-    
-      repos[repoName] = repoCollaborators;
+        return teams;
     }
-    );
-    
-    return repos;
+    //   import { createRequire } from 'node:module';
+    //   const require = createRequire(import.meta.url);
+
+    //   const response = require('./response.json');
+
+    //a function that gets all the repositores and returns their collaborators and their permissions
+    const getRepos = (response)=> {
+        //get the data from the response
+        const data = response;
+
+        //create an object with the repositories and their collaborators
+        const repos = {};
+        //iterate through the repositories
+        data.organization.repositories.edges.forEach((repo) => {
+            //get the repository name
+            const repoName = repo.node.name;
+            //create an object with the collaborators and their permissions
+            const repoCollaborators = {};
+            //iterate through the collaborators
+            repo.node.collaborators.edges.forEach((collaborator) => {
+                //get the collaborator name
+                const collaboratorName = collaborator.node.login;
+                //get the collaborator permissions
+                const collaboratorPermissions = collaborator.permission;
+                //add the collaborator name and permissions to the collaborators object
+                repoCollaborators[collaboratorName] = collaboratorPermissions;
+            });
+
+            repos[repoName] = repoCollaborators;
+        }
+        );
+
+        return repos;
     }
+
+/* harmony default export */ const sort_audit = ({getTeams, getRepos});
+
+
 ;// CONCATENATED MODULE: ./index.js
 
 
--- sort-audit.js
+++ sort-audit.js
@@ -1,55 +1,59 @@
-export function getTeams(response) {
-    //get the data from the response
-    const data = response;
-    //create an object with the teams  their members and repositories
-    const teams = {};
-    const teamPermissions={};
-    //iterate through the teams
-    data.organization.teams.edges.forEach((team) => {
-        //get the team name
-        const teamName = team.node.name;
-      
-        //get the team members
-        teamPermissions.members = team.node.members.edges.map((member) => member.node.login);
-        //get the team repositories
-        teamPermissions.repositories = team.node.repositories.edges.map((repository) => repository.node.name);
-        
-        //add the team name and members to the teams object
-        teams[teamName] = teamPermissions;
-    }); 
-    return teams;
-  }
-//   import { createRequire } from 'node:module';
-//   const require = createRequire(import.meta.url);
-  
-//   const response = require('./response.json');
 
-  //a function that gets all the repositores and returns their collaborators and their permissions
-    export function getRepos(response) {
-    //get the data from the response
-    const data = response;
-        
-    //create an object with the repositories and their collaborators
-    const repos = {};
-    //iterate through the repositories
-    data.organization.repositories.edges.forEach((repo) => {
-      //get the repository name
-      const repoName = repo.node.name;
-      //create an object with the collaborators and their permissions
-        const repoCollaborators = {};
-        //iterate through the collaborators
-        repo.node.collaborators.edges.forEach((collaborator) => {
-        //get the collaborator name
-        const collaboratorName = collaborator.node.login;
-        //get the collaborator permissions
-        const collaboratorPermissions = collaborator.permission;
-        //add the collaborator name and permissions to the collaborators object
-        repoCollaborators[collaboratorName] = collaboratorPermissions;
+   export const getTeams = (response)=>{
+        //get the data from the response
+        const data = response;
+        //create an object with the teams  their members and repositories
+        const teams = {};
+        const teamPermissions = {};
+        //iterate through the teams
+        data.organization.teams.edges.forEach((team) => {
+            //get the team name
+            const teamName = team.node.name;
+
+            //get the team members
+            teamPermissions.members = team.node.members.edges.map((member) => member.node.login);
+            //get the team repositories
+            teamPermissions.repositories = team.node.repositories.edges.map((repository) => repository.node.name);
+
+            //add the team name and members to the teams object
+            teams[teamName] = teamPermissions;
         });
-    
-      repos[repoName] = repoCollaborators;
+        return teams;
     }
-    );
-    
-    return repos;
+    //   import { createRequire } from 'node:module';
+    //   const require = createRequire(import.meta.url);
+
+    //   const response = require('./response.json');
+
+    //a function that gets all the repositores and returns their collaborators and their permissions
+    export const getRepos = (response)=> {
+        //get the data from the response
+        const data = response;
+
+        //create an object with the repositories and their collaborators
+        const repos = {};
+        //iterate through the repositories
+        data.organization.repositories.edges.forEach((repo) => {
+            //get the repository name
+            const repoName = repo.node.name;
+            //create an object with the collaborators and their permissions
+            const repoCollaborators = {};
+            //iterate through the collaborators
+            repo.node.collaborators.edges.forEach((collaborator) => {
+                //get the collaborator name
+                const collaboratorName = collaborator.node.login;
+                //get the collaborator permissions
+                const collaboratorPermissions = collaborator.permission;
+                //add the collaborator name and permissions to the collaborators object
+                repoCollaborators[collaboratorName] = collaboratorPermissions;
+            });
+
+            repos[repoName] = repoCollaborators;
+        }
+        );
+
+        return repos;
     }
+
+export default {getTeams, getRepos};
+
